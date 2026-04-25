c244d56a8fcafefbe054423731b6666475e50beb
kshattuc <kshattuc@villanova.edu>
2026-04-25 18:47:14 -0400

    added more animals

--- src/cats.js
+++ src/cats.js
@@ -6,7 +6,7 @@ const Cats = () => {
       <div className="row mt-5">
         <div className="col-3">
           <p>Available: Yes!</p>
-          <p>Age: 1.5 years</p>
+          <p>Age: 4 months</p>
           <p>Gender: Female</p>
           <p>Breed: Siamese</p>
           <p>Color: Cream/Dark Brown</p>
@@ -27,7 +27,7 @@ const Cats = () => {
       <div className="row mt-5">
         <div className="col-3">
           <p>Available: Yes!</p>
-          <p>Age: 3 years</p>
+          <p>Age: 2 months</p>
           <p>Gender: Male</p>
           <p>Breed: British Shorthair</p>
           <p>Color: Grey/Tan/White</p>
@@ -38,6 +38,48 @@ const Cats = () => {
 
           <img
             src="/images/whiskers.jpg"
+            alt="cat"
+            style={{ width: "50%" }}
+          />
+        </div>
+      </div>
+      <h1>Midnight</h1>
+
+      <div className="row mt-5">
+        <div className="col-3">
+          <p>Available: Yes!</p>
+          <p>Age: 12 weeks</p>
+          <p>Gender: Female</p>
+          <p>Breed: Maine Coon</p>
+          <p>Color: Black</p>
+          <p>House-trained: Yes</p>
+          <p>Spayed/Neutered: Yes</p>
+          <p>Vaccinated: Yes</p>
+          <p>Midnight enjoys nature and walking outside. She is very intelligent and picks up new skills quickly. </p>
+
+          <img
+            src="/images/midnight.png"
+            alt="cat"
+            style={{ width: "50%" }}
+          />
+        </div>
+      </div>
+      <h1>Fibonacci</h1>
+
+      <div className="row mt-5">
+        <div className="col-3">
+          <p>Available: Yes!</p>
+          <p>Age: 6 weeks</p>
+          <p>Gender: Male</p>
+          <p>Breed: Munchkin Cat</p>
+          <p>Color: White/Yellow/Grey</p>
+          <p>House-trained: Yes</p>
+          <p>Spayed/Neutered: Yes</p>
+          <p>Vaccinated: Yes</p>
+          <p>Fibonacci loves to learn new skills. He also loves to curl up by the fire. </p>
+
+          <img
+            src="/images/fibonacci.png"
             alt="cat"
             style={{ width: "50%" }}
           />
--- src/dogs.js
+++ src/dogs.js
@@ -7,15 +7,75 @@ const Dogs = () => {
         <div className="col-3">
           <p>Available: Yes!</p>
           <p>Age: 1 year</p>
-          <p>Gender: female</p>
-          <p>Breed: schnauzer</p>
-          <p>Color: black</p>
+          <p>Gender: Female</p>
+          <p>Breed: Schnauzer</p>
+          <p>Color: Black</p>
           <p>House-trained: Yes</p>
           <p>Spayed/Neutered: Yes</p>
           <p>Vaccinated: Yes</p>
           <p>Sadie is very playful and especially loves to play with frisbees. She loves to drink water and will eat literally anything.</p>
           <img
             src="/images/sadie.png"
+            alt="dog"
+            style={{ width: "50%" }}
+          />
+        </div>
+      </div>
+      <h1>Candy</h1>
+
+      <div className="row mt-5">
+        <div className="col-3">
+          <p>Available: Yes!</p>
+          <p>Age: 12 weeks </p>
+          <p>Gender: Female</p>
+          <p>Breed: Labrador Retriever</p>
+          <p>Color: Yellow</p>
+          <p>House-trained: Yes</p>
+          <p>Spayed/Neutered: Yes</p>
+          <p>Vaccinated: Yes</p>
+          <p>Candy is very affectiate and loves to give you kisses. She will follow you around the house and won't leave you side. </p>
+          <img
+            src="/images/candy.png"
+            alt="dog"
+            style={{ width: "50%" }}
+          />
+        </div>
+      </div>
+        <h1>Eddy</h1>
+
+      <div className="row mt-5">
+        <div className="col-3">
+          <p>Available: Yes!</p>
+          <p>Age: 6 weeks </p>
+          <p>Gender: Male</p>
+          <p>Breed: Dachshunds</p>
+          <p>Color: Brown</p>
+          <p>House-trained: Yes</p>
+          <p>Spayed/Neutered: Yes</p>
+          <p>Vaccinated: Yes</p>
+          <p>Eddy loves to play and go on walks. He loves other animals and is great around kids. </p>
+          <img
+            src="/images/eddy.png"
+            alt="dog"
+            style={{ width: "50%" }}
+          />
+        </div>
+      </div>
+        <h1>Loco</h1>
+
+      <div className="row mt-5">
+        <div className="col-3">
+          <p>Available: Yes!</p>
+          <p>Age: 5 months </p>
+          <p>Gender: Male</p>
+          <p>Breed: Cavalier King Charles Spaniel</p>
+          <p>Color: White/Brown</p>
+          <p>House-trained: Yes</p>
+          <p>Spayed/Neutered: Yes</p>
+          <p>Vaccinated: Yes</p>
+          <p>Loco is a goofball who loves to run in circles and chase his own tail. He loves to play fetch and jump around. </p>
+          <img
+            src="/images/loco.png"
             alt="dog"
             style={{ width: "50%" }}
           />
