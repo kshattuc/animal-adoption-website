e846a16a495256f313a1db24041094d12a668b58
kshattuc <kshattuc@villanova.edu>
2026-04-25 18:47:14 -0400

    added more animals

--- src/other-animals.js
+++ src/other-animals.js
@@ -7,8 +7,8 @@ const OtherAnimals = () => {
         <div className="col-3">
           <p>Available: Yes!</p>
           <p>Age: 1 year</p>
-          <p>Gender: male</p>
-          <p>Type: hognose</p>
+          <p>Gender: Male</p>
+          <p>Type: Hognose</p>
           <p>Color: Brown/Tan/White </p>
           <p>Noodle loves to climb and explore.</p>
           <img
@@ -24,7 +24,7 @@ const OtherAnimals = () => {
         <div className="col-3">
           <p>Available: Yes!</p>
           <p>Age: 1 year</p>
-          <p>Gender: male</p>
+          <p>Gender: Male</p>
           <p>Type: Chameleon</p>
           <p>Color: Multicolored </p>
           <p>Cameron is shy and likes to blend in with his surroundings.</p>
@@ -41,12 +41,29 @@ const OtherAnimals = () => {
         <div className="col-3">
           <p>Available: Yes!</p>
           <p>Age: 6 months</p>
-          <p>Gender: female</p>
+          <p>Gender: Female</p>
           <p>Type: Blue Mustard Betta Fish</p>
           <p>Color: Yellow/Blue </p>
           <p>Marigold is territorial and loves to swim around.</p>
           <img
             src="/images/marigold.png"
+            alt="fish"
+            style={{ width: "50%" }}
+          />
+        </div>
+      </div>
+      <h1>Maria</h1>
+
+      <div className="row mt-5">
+        <div className="col-3">
+          <p>Available: Yes!</p>
+          <p>Age: 6 weeks</p>
+          <p>Gender: Female</p>
+          <p>Type: Hamster</p>
+          <p>Color: Orange/White </p>
+          <p>Maria is silly and loves to roll around in a hamster wheel all day.</p>
+          <img
+            src="/images/maria.png"
             alt="fish"
             style={{ width: "50%" }}
           />
