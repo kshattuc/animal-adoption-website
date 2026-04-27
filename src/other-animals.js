581614b60b1537d4e1f3883196ee0e8c09b05183
kshattuc <kshattuc@villanova.edu>
2026-04-26 22:19:50 -0400

    put dogs and other animals in arrays

--- src/other-animals.js
+++ src/other-animals.js
@@ -1,76 +1,89 @@
-const OtherAnimals = () => {
+import { useState } from "react";
+export default function OtherAnimals() {
+const OtherAnimals = [
+  {
+    id: 1,
+    name: "Noodle",
+    available: true,
+    age: "1 year",
+    breed: "Hognose Snake",
+    color: "Brown/Tan/White",
+    gender: "Male",
+    description:"Noodle loves to climb and explore.",
+    image: "/images/noodle.png"
+  },
+  {
+    id: 2,
+    name: "Cameron",
+    available: true,
+    age: "1 year",
+    breed: "Chameleon",
+    color: "Multicolored",
+    gender: "Male",
+    description:"Cameron is shy and likes to blend in with his surroundings.",
+    image: "/images/cameron.png"
+  },
+  {
+    id: 3,
+    name: "Marigold",
+    available: true,
+    age: "6 months",
+    breed: "Blue Mustard Betta Fish",
+    color: "Yellow/Blue",
+    gender: "female",
+    description:"Marigold is territorial and loves to swim around.",
+    image: "/images/marigold.png"
+  },
+  {
+    id: 4,
+    name: "Maria",
+    available: true,
+    age: "6 weeks",
+    breed: "Hamster",
+    color: "Orange/White",
+    gender: "Female",
+    description:"Maria is silly and loves to roll around in a hamster wheel all day.",
+    image: "/images/maria.png"
+  },
+];
+const [pets, setPets] = useState(OtherAnimals);
+
+  const handleAdopt = (id) => {
+    setPets((prevPets) =>
+      prevPets.map((pet) =>
+        pet.id === id ? { ...pet, available: false } : pet
+      )
+    );
+  };
+
   return (
-    <div className="container">
-      <h1>Noodle</h1>
+    <div className="row mt-5">
+      {pets.map((pet) => (
+        <div className="col-3" key={pet.id}>
+          <h3>{pet.name}</h3>
 
-      <div className="row mt-5">
-        <div className="col-3">
-          <p>Available: Yes!</p>
-          <p>Age: 1 year</p>
-          <p>Gender: Male</p>
-          <p>Type: Hognose</p>
-          <p>Color: Brown/Tan/White </p>
-          <p>Noodle loves to climb and explore.</p>
           <img
-            src="/images/noodle.png"
-            alt="snake"
+            src={pet.image}
+            alt={pet.name}
             style={{ width: "50%" }}
           />
-        </div>
-      </div>
-       <h1>Cameron</h1>
 
-      <div className="row mt-5">
-        <div className="col-3">
-          <p>Available: Yes!</p>
-          <p>Age: 1 year</p>
-          <p>Gender: Male</p>
-          <p>Type: Chameleon</p>
-          <p>Color: Multicolored </p>
-          <p>Cameron is shy and likes to blend in with his surroundings.</p>
-          <img
-            src="/images/cameron.png"
-            alt="chameleon"
-            style={{ width: "50%" }}
-          />
-        </div>
-      </div>
-      <h1>Marigold</h1>
+          <p>Available: {pet.available ? "Yes!" : "No"}</p>
+          <p>Age: {pet.age}</p>
+          <p>Breed: {pet.breed}</p>
+          <p>Color: {pet.color}</p>
+          <p>Gender: {pet.gender}</p>
+          <p>{pet.description}</p>
 
-      <div className="row mt-5">
-        <div className="col-3">
-          <p>Available: Yes!</p>
-          <p>Age: 6 months</p>
-          <p>Gender: Female</p>
-          <p>Type: Blue Mustard Betta Fish</p>
-          <p>Color: Yellow/Blue </p>
-          <p>Marigold is territorial and loves to swim around.</p>
-          <img
-            src="/images/marigold.png"
-            alt="fish"
-            style={{ width: "50%" }}
-          />
+          <button
+            onClick={() => handleAdopt(pet.id)}
+            className="btn btn-success mt-2"
+            disabled={!pet.available}
+          >
+            {pet.available ? "Adopt Now" : "Adopted"}
+          </button>
         </div>
-      </div>
-      <h1>Maria</h1>
-
-      <div className="row mt-5">
-        <div className="col-3">
-          <p>Available: Yes!</p>
-          <p>Age: 6 weeks</p>
-          <p>Gender: Female</p>
-          <p>Type: Hamster</p>
-          <p>Color: Orange/White </p>
-          <p>Maria is silly and loves to roll around in a hamster wheel all day.</p>
-          <img
-            src="/images/maria.png"
-            alt="fish"
-            style={{ width: "50%" }}
-          />
-        </div>
-      </div>
+      ))}
     </div>
-  );
-};
-
-export default OtherAnimals;
+  )
+}
